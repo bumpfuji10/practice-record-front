6d031b35c759ef78a7cbeaebab85da007d4dbda7
Yo Kamada <bumpfuji10@gmail.com>
2024-04-02 22:46:15 +0900

    update: main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import './styles/base.scss';
 import './styles/header.scss';
 import './styles/login.scss';
 import './styles/signup.scss';
-import App from './App.vue'
+import App from './App.vue';
+import router from '../router';
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app');
