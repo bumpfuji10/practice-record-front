6fe152372e9a5f2a10d0331d234268f884bfb808
Yo Kamada <bumpfuji10@gmail.com>
2024-04-03 07:29:24 +0900

    add: header

---
+++ src/components/NotLoginHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>
+      <div class="header-container">
+        <nav class="gyraph-logo">
+          <router-link to="/">Gyraph</router-link>
+        </nav>
+        <div class="loginAndNewAccount">
+          <button class="login">
+            <router-link to="/login">ログイン</router-link>
+          </button>
+          <button class="new-account">
+            <router-link to="/signup">新規登録</router-link>
+          </button>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotLoginHeader'
+}
+</script>
