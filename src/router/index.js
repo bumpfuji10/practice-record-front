03c39b775e14642cecb137554dcad948cce6503a
Yo Kamada <bumpfuji10@gmail.com>
2024-04-04 23:03:19 +0900

    add: login routere

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Signup from "./../components/pages/Signup.vue";
+import Login from "./../components/pages/Login.vue";
 import PracticeRecordIndex from "./../components/pages/PracticeRecordIndex.vue"
 
 const routes = [
   { path: '/', component: PracticeRecordIndex },
-  { path: '/signup', component: Signup }
+  { path: '/signup', component: Signup },
+  { path: '/login', component: Login },
 ];
 
 const router = createRouter({
