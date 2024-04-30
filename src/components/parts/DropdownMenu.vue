698cfeb6411cf3f09cad68221ba3ef72e1947ddc
Yo Kamada <bumpfuji10@gmail.com>
2024-04-30 22:10:44 +0900

    全toggleをdropdownにrename

--- src/components/parts/DropdownMenu.vue
+++ src/components/parts/DropdownMenu.vue
@@ -1,21 +1,21 @@
 <template>
-<div class="setting-toggle" v-if="is_show_toggle">
-  <ul class="toggle-ul">
-    <li class="toggle-list">
-      <img src="/src/assets/user.svg" alt="" class="toggle-list-icon">
+<div class="setting-dropdown" v-if="is_show_dropdown">
+  <ul class="dropdown-ul">
+    <li class="dropdown-list">
+      <img src="/src/assets/user.svg" alt="" class="dropdown-list-icon">
       マイページ
     </li>
-    <li class="toggle-list">
-      <img src="/src/assets/heart.svg" alt="" class="toggle-list-icon">
+    <li class="dropdown-list">
+      <img src="/src/assets/heart.svg" alt="" class="dropdown-list-icon">
       いいねした日誌
     </li>
-    <li class="toggle-list">
-      <img src="/src/assets/pen.svg" alt="" class="toggle-list-icon">
+    <li class="dropdown-list">
+      <img src="/src/assets/pen.svg" alt="" class="dropdown-list-icon">
       日誌を書く
     </li>
     <br>
-    <li class="toggle-list" @click="submitLogout">
-      <img src="/src/assets/logout.svg" alt="" class="toggle-list-icon">
+    <li class="dropdown-list" @click="submitLogout">
+      <img src="/src/assets/logout.svg" alt="" class="dropdown-list-icon">
       ログアウト
     </li>
   </ul>
@@ -26,7 +26,7 @@
 import { logoutUser } from '../../resources/user_logout'
 
 export default {
-  props: ["is_show_toggle"],
+  props: ["is_show_dropdown"],
   methods: {
     submitLogout() {
       try {
